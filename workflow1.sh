b74e5c9cc7389d8502d8d0f26988693aad5bbadd
Tushar D. S. <tushar.ds31@outlook.com>
2024-12-28 09:34:35 +0530

    Update workflow1.sh

--- workflow1.sh
+++ workflow1.sh
@@ -34,10 +34,7 @@ RANDOM_BG_COLOR=${BG_COLORS[$RANDOM % ${#BG_COLORS[@]}]}
 
 #----------------------------------------------------start--------------------------------------------------#
 
-echo "${RANDOM_BG_COLOR}${RANDOM_TEXT_COLOR}${BOLD}Starting Execution${RESET}"
-
 # Step 1: Assigning variables
-echo "${BOLD}${BLUE}Assigning variables${RESET}"
 export PROJECT_ID=$(gcloud config get-value project)
 export ZONE=$(gcloud compute project-info describe \
 --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
@@ -47,13 +44,11 @@ export CLUSTER=hello-cluster
 export REPO=my-repository
 
 # Step 2: Enable required GCP services
-echo "${BOLD}${YELLOW}Enabling Required GCP Services${RESET}"
 gcloud services enable container.googleapis.com \
     cloudbuild.googleapis.com \
     sourcerepo.googleapis.com
 
 # Step 3: Create Artifact Repository
-echo "${BOLD}${CYAN}Creating Artifact Repository${RESET}"
 gcloud artifacts repositories create $REPO \
     --repository-format=docker \
     --location=$REGION \
@@ -62,13 +57,11 @@ gcloud artifacts repositories create $REPO \
 sleep 20
 
 # Step 4: Assign IAM roles to Cloud Build service account
-echo "${BOLD}${MAGENTA}Adding IAM Policy Binding${RESET}"
 gcloud projects add-iam-policy-binding $PROJECT_ID \
 --member=serviceAccount:$(gcloud projects describe $PROJECT_ID \
 --format="value(projectNumber)")@cloudbuild.gserviceaccount.com --role="roles/container.developer"
 
 # Step 5: Install GitHub CLI and configure Git
-echo "${BOLD}${BLUE}Installing GitHub CLI and Configuring Git${RESET}"
 curl -sS https://webi.sh/gh | sh
 gh auth login
 gh api user -q ".login"
@@ -77,17 +70,14 @@ git config --global user.name "${GITHUB_USERNAME}"
 git config --global user.email "${USER_EMAIL}"
 
 # Step 6: Create Kubernetes cluster
-echo "${BOLD}${YELLOW}Creating Kubernetes Cluster${RESET}"
 gcloud beta container --project "$PROJECT_ID" clusters create "$CLUSTER" --zone "$ZONE" --no-enable-basic-auth --cluster-version latest --release-channel "regular" --machine-type "e2-medium" --image-type "COS_CONTAINERD" --disk-type "pd-balanced" --disk-size "100" --metadata disable-legacy-endpoints=true  --logging=SYSTEM,WORKLOAD --monitoring=SYSTEM --enable-ip-alias --network "projects/$PROJECT_ID/global/networks/default" --subnetwork "projects/$PROJECT_ID/regions/$REGION/subnetworks/default" --no-enable-intra-node-visibility --default-max-pods-per-node "110" --enable-autoscaling --min-nodes "2" --max-nodes "6" --location-policy "BALANCED" --no-enable-master-authorized-networks --addons HorizontalPodAutoscaling,HttpLoadBalancing,GcePersistentDiskCsiDriver --enable-autoupgrade --enable-autorepair --max-surge-upgrade 1 --max-unavailable-upgrade 0 --enable-shielded-nodes --node-locations "$ZONE"
 
 # Step 7: Configure Kubernetes
-echo "${BOLD}${CYAN}Configuring Kubernetes${RESET}"
 gcloud container clusters get-credentials hello-cluster --zone=$ZONE
 kubectl create namespace prod
 kubectl create namespace dev
 
 # Step 8: Clone and Configure GitHub Repository
-echo "${BOLD}${MAGENTA}Cloning and Configuring GitHub Repository${RESET}"
 gh repo create sample-app --private
 git clone https://github.com/${GITHUB_USERNAME}/sample-app.git
 cd ~
@@ -112,18 +102,17 @@ git commit -m "Initial commit for dev branch"
 git push origin dev
 
 # Step 9: Output Cloud Build Trigger Link
-echo "${BOLD}${YELLOW}Cloud Build Trigger Link${RESET}"
-echo "${BOLD}${BLUE}Visit this link to configure triggers: ${RESET}" "https://console.cloud.google.com/cloud-build/triggers;region=global/add?project=$PROJECT_ID"
+echo "${BOLD}Visit this link to configure triggers: ${RESET}" "https://console.cloud.google.com/cloud-build/triggers;region=global/add?project=$PROJECT_ID"
 
 # Production trigger info
-echo -e "${BOLD}${YELLOW}Production Deployment Trigger:${RESET}"
+echo -e "${BOLD}Production Deployment Trigger:${RESET}"
 echo -e "${BOLD}Property Value Name:${RESET} sample-app-prod-deploy"
 echo -e "${BOLD}Branch Pattern:${RESET} ^master$"
 echo -e "${BOLD}Build Configuration File:${RESET} cloudbuild.yaml"
 echo ""
 
 # Development trigger info
-echo -e "${BOLD}${YELLOW}Development Deployment Trigger:${RESET}"
+echo -e "${BOLD}Development Deployment Trigger:${RESET}"
 echo -e "${BOLD}Property Value Name:${RESET} sample-app-dev-deploy"
 echo -e "${BOLD}Branch Pattern:${RESET} ^dev$"
 echo -e "${BOLD}Build Configuration File:${RESET} cloudbuild-dev.yaml"
